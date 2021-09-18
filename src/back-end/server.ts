54c02e42195c5e7955a7854849afeb43abf06c0d
s1eepercat <jevlentjev@gmail.com>
2021-09-18 16:25:19 +0200

    heroku fix

--- src/back-end/server.ts
+++ src/back-end/server.ts
@@ -5,4 +5,4 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use('/', express.static('public'));
-app.listen(3000, () => console.log(`Server is on, port ${port}.`));
+app.listen(port, () => console.log(`Server is on, port ${port}.`));
